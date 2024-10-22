6c538bf4b8016f16886823dcedf1642970424de3
Wakoz <julien.cartron.pro@gmail.com>
2024-10-22 12:23:38 +0200

    step quest 3 react

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,8 +1,25 @@
 function PokemonCard() {
-  return <figure>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-    <figcaption>Bulbasaur</figcaption>
-  </figure>;
-}
+  const pokemon = pokemonList[0];
 
+  return (
+    <figure>
+      {pokemon.imgSrc ? (
+        <img src= {pokemon.imgSrc} alt= {pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
+    <figcaption>{pokemon.name}</figcaption>
+  </figure>
+)};
+
+const pokemonList = [
+  {
+    name: "Bulbasaur",
+    imgSrc:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "Mew",
+    imgSrc: ""
+  }
+];
 export default PokemonCard;
