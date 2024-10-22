98d5541fc2ed4d14089c6600fa4c13ba52e9a3af
Wakoz <julien.cartron.pro@gmail.com>
2024-10-22 11:04:06 +0200

    step 2 pokedex

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
 
 import MyTitle from "./components/MyTitle";
+import PokemonCard from './components/PokemonCard';
 
 function App() {
   return (
       <section>
           <MyTitle />
+          <div>
+            <PokemonCard />
+          </div>
       </section>
   );
 }
